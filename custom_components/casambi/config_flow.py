da1e1dbcb2dd1aecbb23dde692d04256cacf3226
Olof Hellqvist <olof.hellqvist@gmail.com>
2021-04-05 15:09:38 +0200

    Testing passwords

--- custom_components/casambi/config_flow.py
+++ custom_components/casambi/config_flow.py
@@ -13,6 +13,7 @@ from homeassistant.const import (
     CONF_SCAN_INTERVAL
 )
 from aiocasambi.helper import Helper
+from aiocasambi.errors import AiocasambiException
 from homeassistant.core import callback
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
@@ -55,7 +56,21 @@ REPO_SCHEMA = vol.Schema(
 
 OPTIONS_SHCEMA = vol.Schema({vol.Optional(CONF_NAME, default="foo"): cv.string})
 
-async def validate_auth(email: str, api_key: str, user_password: str, 
+async def validate_user_password(email: str, api_key: str, user_password: str, 
+    hass: core.HomeAssistant) -> None:
+    """Validates a GitHub access token.
+
+    Raises a ValueError if the auth token is invalid.
+    """
+    session = async_get_clientsession(hass)
+    helper = Helper(email=email, api_key=api_key, websession=session)
+
+    try:
+        await helper.test_user_password(user_password)
+    except AiocasambiException:
+        raise ValueError
+
+async def validate_network_password(email: str, api_key: str,
     network_password: str, hass: core.HomeAssistant) -> None:
     """Validates a GitHub access token.
 
@@ -64,6 +79,10 @@ async def validate_auth(email: str, api_key: str, user_password: str,
     session = async_get_clientsession(hass)
     helper = Helper(email=email, api_key=api_key, websession=session)
 
+    try:
+        await helper.test_network_password(network_password)
+    except AiocasambiException:
+        raise ValueError
 
 class CasambiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Github Custom config flow."""
@@ -76,9 +95,15 @@ class CasambiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             _LOGGER.debug(f"async_step_user user_input: {user_input}")
             try:
-                await validate_auth(user_input[CONF_EMAIL], user_input[CONF_API_KEY], user_input[CONF_USER_PASSWORD], user_input[CONF_NETWORK_PASSWORD], self.hass)
+                await validate_user_password(user_input[CONF_EMAIL], user_input[CONF_API_KEY], user_input[CONF_USER_PASSWORD], self.hass)
             except ValueError:
-                errors["base"] = "auth"
+                errors["base"] = "auth_user_password"
+
+            try:
+                await validate_network_password(user_input[CONF_EMAIL], user_input[CONF_API_KEY], user_input[CONF_NETWORK_PASSWORD], self.hass)
+            except ValueError:
+                errors["base"] = "auth_network_password"
+            
             if not errors:
                 # Input is valid, set data.
                 self.data = user_input
