94afb99e9ca51d3c4a8478f993ccc0330745e508
Olof Hellqvist <olof.hellqvist@gmail.com>
2021-04-10 09:00:34 +0200

    Updating

--- custom_components/casambi/const.py
+++ custom_components/casambi/const.py
@@ -12,6 +12,9 @@ DOMAIN = "casambi"
 
 WIRE_ID = 9
 
+DEFAULT_NETWORK_TIMEOUT = 300
+DEFAULT_POLLING_TIME = 60
+
 CONF_USER_PASSWORD = "user_password"
 CONF_NETWORK_PASSWORD = "network_password"
 CONF_NETWORK_TIMEOUT = 'network_timeout'
@@ -27,7 +30,7 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_NETWORK_PASSWORD): cv.string,
         vol.Required(CONF_EMAIL): cv.string,
         vol.Required(CONF_API_KEY): cv.string,
-        vol.Optional(CONF_NETWORK_TIMEOUT, default=300): cv.positive_int,
-        vol.Optional(CONF_SCAN_INTERVAL, default=60): cv.positive_int,
+        vol.Optional(CONF_NETWORK_TIMEOUT, default=DEFAULT_NETWORK_TIMEOUT): cv.positive_int,
+        vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_POLLING_TIME): cv.positive_int,
     })
 }, extra=vol.ALLOW_EXTRA)
