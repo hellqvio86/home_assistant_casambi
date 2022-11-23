ac0416bb6344a73562645f60dadb09ed572fdc56
Christopher Fenner <CFenner@users.noreply.github.com>
2022-11-23 22:42:57 +0100

    Create binary_sensor.py

---
+++ custom_components/casambi/binary_sensor.py
@@ -0,0 +1,36 @@
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .casambi.CasambiStatusBinarySensorEntity import CasambiStatusBinarySensorEntity
+from .const import (
+    DOMAIN,
+    CONF_CONTROLLER,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities):
+    _LOGGER.debug("Setting up binary sensor entities.")
+    # config = hass.data[DOMAIN][config_entry.entry_id]
+
+    # casambi_controller = hass.data[DOMAIN][config_entry.entry_id][CONF_CONTROLLER]
+    # controller = casambi_controller.controller
+    # units = controller.get_units()
+    units = []
+
+    binarySensors = []
+
+    for unit in units:
+        _LOGGER.debug("Adding CasambiStatusBinarySensorEntity...")
+        binarySensors.append(CasambiStatusBinarySensorEntity(unit, controller, hass))
+
+    if binarySensors:
+        _LOGGER.debug("Adding binary sensor entities...")
+        async_add_entities(binarySensors)
+    else:
+        _LOGGER.debug("No binary sensor entities available.")
