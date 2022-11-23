cc4454e82e6abaa5e1663d5af435646e452ac06e
Christopher Fenner <CFenner@users.noreply.github.com>
2022-11-23 22:30:43 +0100

    Update CasambiStatusBinarySensorEntity.py

--- custom_components/casambi/casambi/CasambiStatusBinarySensorEntity.py
+++ custom_components/casambi/casambi/CasambiStatusBinarySensorEntity.py
@@ -1,8 +1,9 @@
-
 import logging
 
-from .CasambiBinarySensorEntity import CasambiBinarySensorEntity
+from homeassistant.components.binary_sensor import BinarySensorDeviceClass
+from homeassistant.helpers.entity import EntityCategory
 
+from .CasambiBinarySensorEntity import CasambiBinarySensorEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -10,53 +11,20 @@ _ENTITY_SUFFIX = "Status"
 
 class CasambiStatusBinarySensorEntity(CasambiBinarySensorEntity):
     def __init__(self, unit, controller, hass):
-        CasambiBinarySensorEntity.__init__(
-            self, unit, controller, hass,
-            _ENTITY_SUFFIX,
-            # "mdi:google-lens",
-        )
+        _LOGGER.debug(f"Casambi {name_suffix} binary sensor - init - start")
 
-        # self._attr_icon = "mdi:light-flood-down"
+        self._hass = hass
+        self._attr_icon = icon
+        self._attr_device_class = BinarySensorDeviceClass.CONNECTIVITY
+        self._attr_is_on = False
+        CasambiBinarySensorEntity.__init__(self, unit, controller, hass, _ENTITY_SUFFIX)
 
-    def update_state(self):
-        """
-        Update units state
-        """
-        if self.enabled:
-            # Device needs to be enabled for us to schedule updates
-            _LOGGER.debug(f"update_state {self}")
-            self.async_schedule_update_ha_state(True)
+        _LOGGER.debug(f"Casambi {name_suffix} binary sensor - init - end")
 
-    # def process_update(self, data):
-    #     """Process callback message, update home assistant light state"""
-    #     _LOGGER.debug(f"process_update: self: {self} data: {data}")
-
-    #     if self.enabled:
-    #         # Device needs to be enabled for us to schedule updates
-    #         self.async_schedule_update_ha_state(True)
-
-    async def async_update(self) -> None:
-        """Update Casambi entity."""
-        _LOGGER.info(f"async_update: unit is not online: {self}")
-
-        if not self.unit.online:
-          self._attr_state = True
-        else:
-          self._attr_state = False
-
-        # else:
-            # if self.unit.value > 0:
-                # self._state = True
-                # self._brightness = int(round(self.unit.value * 255))
-                # self._distribution = int(round(self.unit.distribution * 255))
-            # else:
-            #     self._state = False
-        _LOGGER.debug(f"async_update {self}")
+    @property
+    def entity_category(self):
+        return EntityCategory.DIAGNOSTICS
 
     def __repr__(self) -> str:
         """Return the representation."""
-        name = self.unit.name
-
-        result = f"<Casambi {_ENTITY_SUFFIX} binary sensor {name}: unit={self.unit}"
-
-        return result
+        return f"<Casambi {_ENTITY_SUFFIX} binary sensor {self.unit.name}: unit={self.unit}"
