f7aa80b4bbd945495083ee335d67ffbf21e8111d
Christopher Fenner <CFenner@users.noreply.github.com>
2022-11-26 18:13:52 +0100

    Update CasambiBinarySensorEntity.py

--- custom_components/casambi/casambi/CasambiBinarySensorEntity.py
+++ custom_components/casambi/casambi/CasambiBinarySensorEntity.py
@@ -9,19 +9,17 @@ from .CasambiEntity import CasambiEntity
 _LOGGER = logging.getLogger(__name__)
 
 class CasambiBinarySensorEntity(BinarySensorEntity, CasambiEntity):
-    def __init__(self, unit, controller, hass, name_suffix: str = "", icon=None, device_class=None):
-        _LOGGER.debug(f"Casambi {name_suffix} - init - start")
+    def __init__(self, unit, controller, hass, name: str = None, device_class=None, icon=None):
+        _LOGGER.debug(f"Casambi {name} - init - start")
 
-        self._attr_is_on = False
-        self._hass = hass
-        self._attr_icon = icon
-        self._attr_device_class = device_class
-
-        CasambiEntity.__init__(self, unit, controller, hass)
         BinarySensorEntity.__init__(self)
+        CasambiEntity.__init__(self, unit, controller, hass, name)
+        self._attr_is_on = False
+        self._attr_device_class = device_class
+        self._attr_icon = icon
 
-        _LOGGER.debug(f"Casambi {name_suffix} - init - end")
+        _LOGGER.debug(f"Casambi {name} - init - end")
 
-    @property
-    def state(self):
-        return self._attr_state
+    # @property
+    # def state(self):
+    #     return self._attr_state
