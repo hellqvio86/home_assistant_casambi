6461d5e83f56f5ad75b013bbb99a03f84a958896
Christopher Fenner <CFenner@users.noreply.github.com>
2022-11-26 18:21:12 +0100

    Update binary_sensor.py

--- custom_components/casambi/binary_sensor.py
+++ custom_components/casambi/binary_sensor.py
@@ -3,8 +3,8 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .casambi.CasambiStatusBinarySensorEntity import CasambiStatusBinarySensorEntity
 from .casambi.CasambiOverheatBinarySensorEntity import CasambiOverheatBinarySensorEntity
+from .casambi.CasambiStatusBinarySensorEntity import CasambiStatusBinarySensorEntity
 from .const import (
     DOMAIN,
     CONF_CONTROLLER,
@@ -20,25 +20,27 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, asyn
     # config = hass.data[DOMAIN][config_entry.entry_id]
 
     # TODO: extract casambi controller from light entity
-    # casambi_controller = hass.data[DOMAIN][config_entry.entry_id][CONF_CONTROLLER]
-    # controller = casambi_controller.controller
-    # units = controller.get_units()
-    units = []
+    casambi_controller = hass.data[DOMAIN][CONF_CONTROLLER]
+    coordinator = hass.data[DOMAIN]["coordinator"]
+    await coordinator.async_refresh()
 
+    units = casambi_controller.controller.get_units()
     binarySensors = []
 
     for unit in units:
         _LOGGER.debug("Adding CasambiStatusBinarySensorEntity...")
-        binarySensors.append(CasambiStatusBinarySensorEntity(unit, controller, hass))
-        
+        binarySensors.append(CasambiStatusBinarySensorEntity(unit, casambi_controller.controller, hass))
+
         # TODO: check for overheat control
         # 'controls': [[{'name': 'overheat', 'type': 'Overheat', 'status': 'ok'}, {'name': 'dimmer0', 'type': 'Dimmer', 'value': 0.0}]]
         # if ...
-        #    _LOGGER.debug("Adding CasambiOverheatBinarySensorEntity...")
-        #    binarySensors.append(CasambiOverheatBinarySensorEntity(unit, controller, hass))
+          #  _LOGGER.debug("Adding CasambiOverheatBinarySensorEntity...")
+          #  binarySensors.append(CasambiOverheatBinarySensorEntity(unit, casambi_controller.controller, hass))
 
     if binarySensors:
         _LOGGER.debug("Adding binary sensor entities...")
         async_add_entities(binarySensors)
     else:
         _LOGGER.debug("No binary sensor entities available.")
+
+    return True
