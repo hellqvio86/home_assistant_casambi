399c1cf7001d3c82907824f97db0657eab11f09e
Christopher Fenner <CFenner@users.noreply.github.com>
2022-11-27 23:13:36 +0100

    Update CasambiEntity.py

--- custom_components/casambi/casambi/CasambiEntity.py
+++ custom_components/casambi/casambi/CasambiEntity.py
@@ -6,60 +6,51 @@ import logging
 from typing import Any, Dict
 
 from homeassistant.const import ATTR_NAME
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import DeviceInfo, Entity
 
-from ..const import (
-    DOMAIN,
-    ATTR_IDENTIFIERS,
-    ATTR_MANUFACTURER,
-    ATTR_MODEL,
-)
+from ..const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-
 class CasambiEntity(Entity):
     """Defines a Casambi Entity."""
+    _attr_has_entity_name = True
 
-    def __init__(self, unit, controller, hass):
+    def __init__(self, unit, controller, hass, name: str = None):
         """Initialize Casambi Entity."""
+        _LOGGER.debug(f"Casambi entity - init - start")
         self.unit = unit
         self.controller = controller
         self.hass = hass
+        self._unit_unique_id = unit.unique_id
+        self._attr_name = name
 
-    @property
-    def name(self) -> str:
-        """Return the name of the entity."""
-        return self.unit.name
+        controller.entities.append(self)
+
+        _LOGGER.debug(f"Casambi entity - init - end")
 
     @property
     def unique_id(self) -> str:
         """Return the unique ID for this sensor."""
-        return self.unit.unique_id
+        name = self.unit.unique_id
+        if self._attr_name:
+            name += f"_{self._attr_name}"
+        return name.lower()
 
     @property
-    def model(self):
-        """Return the model for this sensor."""
-        if self.unit.fixture_model:
-            return self.unit.fixture_model
-        return "Casambi"
-
-    @property
-    def brand(self):
-        """Return the brand for this sensor."""
-        if self.unit.oem:
-            return self.unit.oem
-        return "Casambi"
-
-    @property
-    def device_info(self) -> Dict[str, Any]:
+    def device_info(self) -> DeviceInfo:
         """Return device information about this Casambi Key Light."""
-        return {
-            ATTR_IDENTIFIERS: {(DOMAIN, self.unique_id)},
-            ATTR_NAME: self.unit.name,
-            ATTR_MANUFACTURER: self.brand,
-            ATTR_MODEL: self.model,
-        }
+        _LOGGER.debug(f"DeviceInfo: unit: {self.unit}")
+        return DeviceInfo(
+            identifiers = {(DOMAIN, self.unit.unique_id)},
+            # default_name = "Casambi",
+            name = self.unit.name,
+            default_manufacturer = "Casambi",
+            manufacturer = self.unit.oem,
+            default_model = "Casambi",
+            model = self.unit.fixture_model,
+            sw_version = self.unit.firmware_version,
+        )
 
     @property
     def available(self) -> bool:
@@ -70,3 +61,7 @@ class CasambiEntity(Entity):
     def should_poll(self):
         """Disable polling by returning False"""
         return False
+
+    def __repr__(self) -> str:
+        """Return the representation."""
+        return f"<Casambi {self.unit.name}: unit={self.unit}"
