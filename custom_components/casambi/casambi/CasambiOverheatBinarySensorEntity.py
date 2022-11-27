1fdbc45e4ff72b654857ecb7298c7ed14cd66546
Christopher Fenner <CFenner@users.noreply.github.com>
2022-11-27 23:06:39 +0100

    Update CasambiOverheatBinarySensorEntity.py

--- custom_components/casambi/casambi/CasambiOverheatBinarySensorEntity.py
+++ custom_components/casambi/casambi/CasambiOverheatBinarySensorEntity.py
@@ -6,17 +6,15 @@ from .CasambiBinarySensorEntity import CasambiBinarySensorEntity
 
 _LOGGER = logging.getLogger(__name__)
 
-NAME = "Overheat"
-
 class CasambiOverheatBinarySensorEntity(CasambiBinarySensorEntity):
     def __init__(self, unit, controller, hass):
-        _LOGGER.debug(f"Casambi {NAME} binary sensor - init - start")
+        _LOGGER.debug(f"Casambi overheat binary sensor - init - start")
 
         CasambiBinarySensorEntity.__init__(self, unit, controller, hass,
-            NAME, BinarySensorDeviceClass.HEAT)
+            "Overheat", BinarySensorDeviceClass.HEAT)
 
-        _LOGGER.debug(f"Casambi {NAME} binary sensor - init - end")
+        _LOGGER.debug(f"Casambi overheat binary sensor - init - end")
 
     def __repr__(self) -> str:
         """Return the representation."""
-        return f"<Casambi {NAME} binary sensor {self.unit.name}: unit={self.unit}"
+        return f"<Casambi overheat binary sensor {self.unit.name}: unit={self.unit}"
