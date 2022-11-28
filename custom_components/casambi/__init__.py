54b784d0b8ccc6f74f7c9ecdcab9ff70dc019c2a
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2022-11-28 19:36:28 +0100

    refactor: init casabi controller in init function (#74)
    refactor: init casabi controller in init function (#74)
    
    Co-authored-by: Christopher Fenner <CFenner@users.noreply.github.com>

--- custom_components/casambi/__init__.py
+++ custom_components/casambi/__init__.py
@@ -5,33 +5,57 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.issue_registry import async_create_issue, IssueSeverity
 
-from .const import DOMAIN
+from .utils import async_create_controller, async_create_coordinator
+
+from .const import (
+    DOMAIN,
+    CONF_CONTROLLER,
+    CONF_COORDINATOR,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up platform from a ConfigEntry."""
     hass.data.setdefault(DOMAIN, {})
-    hass_data = dict(entry.data)
+    config = hass.data[DOMAIN][config_entry.entry_id] = dict(config_entry.data)
     # Registers update listener to update config entry when options are updated.
-    unsub_options_update_listener = entry.add_update_listener(options_update_listener)
     # Store a reference to the unsubscribe function to cleanup if an entry is unloaded.
-    hass_data["unsub_options_update_listener"] = unsub_options_update_listener
-    hass.data[DOMAIN][entry.entry_id] = hass_data
+    config["remove_update_listener"] = config_entry.add_update_listener(options_update_listener)
+
+    if CONF_CONTROLLER in hass.data[DOMAIN]:
+        dbg_msg = "async_setup_platform CasambiController already created!"
+        _LOGGER.debug(dbg_msg)
+
+        async_create_issue(
+            hass=hass,
+            domain=DOMAIN,
+            issue_id="restart_required_casambi",
+            is_fixable=True,
+            issue_domain=DOMAIN,
+            severity=IssueSeverity.WARNING,
+            translation_key="restart_required",
+            translation_placeholders={},
+        )
+        return True
+
+    controller = hass.data[DOMAIN][CONF_CONTROLLER] = await async_create_controller(hass, config)
+    if not controller:
+        return False
+    hass.data[DOMAIN][CONF_COORDINATOR] = await async_create_coordinator(hass, config, controller)
 
     # Forward the setup to the sensor platform.
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, Platform.LIGHT)
+        hass.config_entries.async_forward_entry_setup(config_entry, Platform.LIGHT)
     )
     return True
-
 
 async def options_update_listener(hass: HomeAssistant, config_entry: ConfigEntry):
     """Handle options update."""
     await hass.config_entries.async_reload(config_entry.entry_id)
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
@@ -41,7 +65,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         )
     )
     # Remove options_update_listener.
-    hass.data[DOMAIN][entry.entry_id]["unsub_options_update_listener"]()
+    hass.data[DOMAIN][entry.entry_id]["remove_update_listener"]()
 
     # Remove config entry from domain.
     if unload_ok:
