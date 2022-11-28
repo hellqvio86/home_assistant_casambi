8ab4039faab63ae6efbcd937272363ac07ba8c5c
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2022-11-28 21:21:19 +0100

    Update binary_sensor.py

--- custom_components/casambi/binary_sensor.py
+++ custom_components/casambi/binary_sensor.py
@@ -3,7 +3,7 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .casambi.CasambiOverheatBinarySensorEntity import CasambiOverheatBinarySensorEntity
+# from .casambi.CasambiOverheatBinarySensorEntity import CasambiOverheatBinarySensorEntity
 from .casambi.CasambiStatusBinarySensorEntity import CasambiStatusBinarySensorEntity
 from .const import (
     DOMAIN,
@@ -18,19 +18,19 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities):
     _LOGGER.debug("Setting up binary sensor entities.")
 
-    casambi_controller = hass.data[DOMAIN][CONF_CONTROLLER]
-    units = casambi_controller.aiocasambi_controller.get_units()
+    controller = hass.data[DOMAIN][CONF_CONTROLLER]
+    units = controller.aiocasambi_controller.get_units()
     binary_sensors = []
 
     for unit in units:
         _LOGGER.debug("Adding CasambiStatusBinarySensorEntity...")
-        binary_sensors.append(CasambiStatusBinarySensorEntity(unit, casambi_controller, hass))
+        binary_sensors.append(CasambiStatusBinarySensorEntity(unit, controller, hass))
         
         # TODO: check for overheat control
         # 'controls': [[{'name': 'overheat', 'type': 'Overheat', 'status': 'ok'}, {'name': 'dimmer0', 'type': 'Dimmer', 'value': 0.0}]]
         # if ...
           #  _LOGGER.debug("Adding CasambiOverheatBinarySensorEntity...")
-          #  binary_sensors.append(CasambiOverheatBinarySensorEntity(unit, casambi_controller, hass))
+          #  binary_sensors.append(CasambiOverheatBinarySensorEntity(unit, controller, hass))
 
     if binary_sensors:
         _LOGGER.debug("Adding binary sensor entities...")
